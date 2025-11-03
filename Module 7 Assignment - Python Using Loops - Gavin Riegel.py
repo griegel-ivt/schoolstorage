d5bbc38c6c72b81a7c263b6dc3a40df55174261b
griegel-ivt <griegel@ivytech.edu>
2025-11-03 18:21:32 -0500

    Delete Module 7 Assignment - Python Using Loops - Gavin Riegel.py

--- Module 7 Assignment - Python Using Loops - Gavin Riegel.py
+++
@@ -1,9 +0,0 @@
-#Array of 15 test numbers
-numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
-
-#For every list entry, if it is divisible by two, it is even, otherwise odd
-for number in numbers:
-    if (number % 2 == 0):
-        print(str(number) + " is even")
-    else:
-        print(str(number) + " is odd")
